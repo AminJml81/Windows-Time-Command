6a9efaece15fd58bacf001970ff71a9919903696
Amin <Aminbest1381@gmail.com>
2025-12-03 23:44:25 +0330

    added arguments feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psutil
 from psutil._common import pcputimes
 import time
+import argparse
 
 
 def calculate_parameters(t1_cpu_times:pcputimes, t1_time:time, t2_cpu_times:pcputimes, t2_time:time):
@@ -18,8 +19,11 @@ def calculate_parameters(t1_cpu_times:pcputimes, t1_time:time, t2_cpu_times:pcpu
     delta_user = t2_cpu_times.user - t1_cpu_times.user
     delta_system = t2_cpu_times.system - t1_cpu_times.system
     delta_total_cpu = delta_user + delta_system
-    cpu_efficiency = (delta_total_cpu / elapsed_wall_time) * 100
-    system_efficiency = (delta_system / elapsed_wall_time) * 100
+    if elapsed_wall_time:
+        cpu_efficiency = (delta_total_cpu / elapsed_wall_time) * 100
+        system_efficiency = (delta_system / elapsed_wall_time) * 100
+    else:
+        cpu_efficiency = system_efficiency = 0
 
     return  elapsed_wall_time, delta_user, delta_system, delta_total_cpu, cpu_efficiency, system_efficiency
 
@@ -39,17 +43,40 @@ def print_parameters(process_name:str, interval:int, elapsed_wall_time:float, de
     print("-" * 40)
      
 
-def wtime_run():
-    process_name = input('Process Name: ').lower()
-    interval = int(input('Interval: '))
-    for proc in psutil.process_iter(["pid", "name"]):
-            if proc.info["name"].lower() == process_name:
-                process_name = proc.info["name"]
+def parse_args():
+    parser = argparse.ArgumentParser(
+         description= "Monitor CPU usage metrics for a specified process over a given time interval."
+    )
+    parser.add_argument('-p', '--process-name', type=str, help="Name of the process to monitor")
+    parser.add_argument('-i', '--interval', type=int, help="Interval in seconds")
+    return parser.parse_args()
 
+def handle_arguments(args):
+    parameters = dict()
+    parameters['process_name'] = args.process_name if args.process_name else input('Process Name: ').lower()
+
+    if args.interval:
+        interval = args.interval
+    else:
+        try:
+            interval = int(input('interval: '))
+        except ValueError:
+            print('Invalid Interval. Defaulting to 5 seconds.')
+            interval = 5
+    
+    parameters['interval'] = interval
+    return parameters
+
+def wtime_run():
+    args = parse_args()
+    parameters = handle_arguments(args)
+
+    for proc in psutil.process_iter(["pid", "name"]):
+            if proc.info["name"].lower() == parameters['process_name']:
                 # getting data of current time(start of the interval) of the process.
                 t1_cpu_times = proc.cpu_times()
                 t1_time = time.time()
-                time.sleep(interval)
+                time.sleep(parameters['interval'])
                 # getting data of the end of the interval of the process.
                 t2_cpu_times = proc.cpu_times()
                 t2_time = time.time()
@@ -58,8 +85,10 @@ def wtime_run():
                     t1_cpu_times, t1_time, t2_cpu_times, t2_time
                 )
 
-                print_parameters(process_name, interval, elapsed_wall_time, delta_user, delta_system, delta_total_cpu, cpu_efficiency, system_efficiency)
+                print_parameters(
+                    parameters['process_name'], parameters['interval'], elapsed_wall_time, delta_user, delta_system,
+                                                delta_total_cpu, cpu_efficiency, system_efficiency
+                )
 
-            
-    
-wtime_run()
+if __name__=='__main__':
+    wtime_run()
