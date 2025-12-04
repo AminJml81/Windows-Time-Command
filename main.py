2a962f0cee12de285e06c89b16ad1b3072922348
Amin <Aminbest1381@gmail.com>
2025-12-05 01:18:31 +0330

    feature/pid selection

--- main.py
+++ main.py
@@ -15,20 +15,25 @@ class WTime:
     )
         parser.add_argument('-p', '--process-name', type=str, help="Name of the process to monitor")
         parser.add_argument('-i', '--interval', type=int, help="Interval in seconds")
+        parser.add_argument('-l', '--list', action='store_true', help='List all PIDs for the given process name and exit.')
+        parser.add_argument('-id', '--pid', type=int, help='Specify the exact PID to monitor.')
         self.args = parser.parse_args()
 
     def handle_arguments(self):
         self.process_name = self.args.process_name if self.args.process_name else input('Process Name: ').lower()
 
-        if self.args.interval:
-            self.interval = self.args.interval
-        else:
-            try:
-                self.interval = int(input('interval: '))
-            except ValueError:
-                print('Invalid Interval. Defaulting to 5 seconds.')
-                self.interval = 5
+        if not self.args.list:
+            if self.args.interval:
+                self.interval = self.args.interval
+            else:
+                try:
+                    self.interval = int(input('interval: '))
+                except ValueError:
+                    print('Invalid Interval. Defaulting to 5 seconds.')
+                    self.interval = 5
 
+        self.pid = self.args.pid if self.args.pid else 0
+            
     def find_process(self):
         processes = []
         for process in psutil.process_iter(['pid', 'name']):
@@ -36,24 +41,61 @@ class WTime:
                 processes.append(process)
 
         return processes
-
-    def run(self):
-        self.parse_args()
-        self.handle_arguments()
-        processes = self.find_process()
-
+    
+    
+    def list_processes(self, processes):
+        print(f'Found pids for {self.process_name}: ')
+        print()
+        print("-" * 40)        
         for process in processes:
-                self.t1_cpu_times = process.cpu_times()
-                self.t1_time = time.time()
-                time.sleep(self.interval)
+            print(f"pid: {process.info['pid']} | Name: {process.info['name']}")
 
-                # getting data of the end of the interval of the process.
-                self.t2_cpu_times = process.cpu_times()
-                self.t2_time = time.time()
+    def get_target_process(self, pid, processes):
+        """returns first process if the pid is not provided, else returns the process with the matching pid."""
+        if pid == 0:
+            self.pid = processes[0].info['pid']
+            return processes[0]
+        else:
+            for process in processes:
+                if process.info['pid'] == pid:
+                    return process
+            return None
+        
+    def run(self):
+        try:
+            self.parse_args()
+            self.handle_arguments()
+            processes = self.find_process()
 
-                self.calculate_parameters()
+            if len(processes) == 0:
+                print(f"No '{self.process_name}' process found.")
+            else:
+                if self.args.list:
+                    self.list_processes(processes)
 
-                self.print_parameters()
+                else:
+                    process = self.get_target_process(self.pid, processes)
+                    if process:
+                        self.t1_cpu_times = process.cpu_times()
+                        self.t1_time = time.time()
+                        time.sleep(self.interval)
+
+                        # getting data of the end of the interval of the process.
+                        self.t2_cpu_times = process.cpu_times()
+                        self.t2_time = time.time()
+
+                        self.calculate_parameters()
+
+                        self.print_parameters()
+                    else:
+                        print(f"Found No '{self.process_name}' process with pid {self.pid}.")
+
+        except KeyboardInterrupt:
+            print('Monitoring stopped by user.')
+        
+        except Exception as e:
+            print(f'Monitoring stopped by following Error: {e}')
+
 
     def calculate_parameters(self):
         self.elapsed_wall_time = self.t2_time - self.t1_time
@@ -69,7 +111,7 @@ class WTime:
 
     def print_parameters(self):
         print("-" * 40)
-        print(f" Results for '{self.process_name}' over {self.interval}s:")
+        print(f"Results for '{self.process_name}' with pid {self.pid} over {self.interval}s:")
         print("-" * 40)
         print(f"Elapsed Time (Wall Clock) : {self.elapsed_wall_time:.4f} sec")
         print(f"CPU Time (User + Sys)     : {self.delta_total_cpu:.4f} sec")
